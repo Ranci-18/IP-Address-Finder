56787584731c739d89e18fd656df232f1686e2a9
Frank <wanyoike39@gmail.com>
2024-08-26 22:20:35 +0300

    add sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Info from './components/Info';
+import './sass/index.sass';
 
 
 const App: React.FC = () => {
   return (
-    <div id='app'>
-        <h1>IP Finder</h1>
+    <div id='app' style={{padding: '20px'}}>
+        <h1 style={{textAlign: 'center',}}>IP Finder</h1>
         <Info />
     </div>
   );
