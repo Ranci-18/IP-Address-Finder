2b63cce9e19292f4217e819912cede9dcb3e8564
Frank <wanyoike39@gmail.com>
2024-08-27 21:08:30 +0300

    fix fetch logic to work as expected

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -9,42 +9,43 @@ const Info: React.FC = () => {
 
     // interface for the IP info
     interface IpInfo {
-        query: string,
-        country: string,
+        country_name: string,
         city: string,
-        lat: number,
-        lon: number,
-        isp: string
+        latitude: number,
+        longitude: number,
+        org: string
     }
     
     // type guard for the IP info
     const isIpInfo = (data: any): data is IpInfo => {
         return data &&
-            typeof data.query === 'string' &&
             typeof data.city === 'string' &&
-            typeof data.lat === 'number' &&
-            typeof data.lon === 'number' &&
-            typeof data.country === 'string';
+            typeof data.latitude === 'number' &&
+            typeof data.longitude === 'number' &&
+            typeof data.country_name === 'string';
     }
 
     // fetch
     const getIpInfo = async (): Promise<void> => {
+        /*if (!ip) {
+            alert('Please enter an IP address');
+            return;
+        }*/
         try {
-            const response = await fetch(`http://ip-api.com/json/${ip}`);
+            const response = await fetch(`https://ipapi.co/${ip}/json/`);
             if (response.ok) {
                 console.log('Response OK');
             }
             const data = await response.json();
-            console.log(data);
+            // console.log(data);
             if (isIpInfo(data)) {
                 setInfo(data);
+                }
+            } catch (error) {
+                console.log('Error:', error);
             }
-            
-        } catch (error) {
-            console.error(error);
-        }
     }
-    
+    console.log(info);
     return (
         <div id='info'>
             <div id="output1">
@@ -61,13 +62,13 @@ const Info: React.FC = () => {
                 {info && 
                 (<>
                     <h3><u>Your IP's location</u></h3>
-                    <p><b>Approximate location: </b><u>{info?.city}, {info?.country}</u></p>
-                    <p><b>ISP: </b><u>{info?.isp}</u></p>
-                    <p><b>Latitude: </b><u>{info?.lat}</u> <b>Longitude: </b><u>{info?.lon}</u></p>
+                    <p><b>Approximate location: </b><u>{info?.city}, {info?.country_name}</u></p>
+                    <p><b>ISP: </b><u>{info?.org}</u></p>
+                    <p><b>Latitude: </b><u>{info?.latitude}</u> <b>Longitude: </b><u>{info?.longitude}</u></p>
                 </>)}
             </div>
             <div id="output2">
-                <Map latitude={info?.lat || 0} longitude={info?.lon || 0} />
+                <Map latitude={info?.latitude || 0} longitude={info?.longitude || 0} />
             </div>
         </div>
     );
